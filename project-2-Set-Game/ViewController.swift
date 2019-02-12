7a00d6e06639446a519483c02689f0c1ca839752
Tomer <tomerkobcherrypick@gmail.com>
2019-02-12 18:18:40 +0200

    Initial Commit

---
+++ project-2-Set-Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  project-2-Set-Game
+//
+//  Created by Tomer Kobrinsky on 12/02/2019.
+//  Copyright © 2019 Tomer Kobrinsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
