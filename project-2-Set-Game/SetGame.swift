69183bff85693cbfad78ba82c6f8aae4f96d91af
Tomer <tomerkobcherrypick@gmail.com>
2019-02-14 14:22:00 +0200

    added tests for set game class

--- project-2-Set-Game/SetGame.swift
+++ project-2-Set-Game/SetGame.swift
@@ -9,36 +9,49 @@
 import Foundation
 
 class SetGame {
-    private var selectedCards = [Card]()
-    private var deck = [Card]()
-    private var cardsOnBoard = [Card]()
-    private var selectedCardsMatched = false
+    private(set) var selectedCards = [Card]()
+    private(set) var deck = [Card]()
+    private(set) var cardsOnBoard = [Card]()
+    private(set) var selectedCardsMatched: Bool?
     init() {
         createDeck()
         deck.shuffle()
         dealTwelveCards()
     }
-    private func selectCard(at index: Int) {
+    public func selectCard(at index: Int) {
+        let cardToChoose = cardsOnBoard[index]
+        // 3 cards are selected
         if selectedCards.count == 3 {
-            if selectedCardsMatched {
-                replaceMatchedCards()
+            //if current selected cards are matched replace these cards in cardsOnBoard
+            if selectedCardsMatched != nil && selectedCardsMatched! {
+                replaceMatchedCards(chosenCards: selectedCards)
             }
-            selectedCards = []
-            selectedCardsMatched = false
-            selectedCards.append(cardsOnBoard[index])
-        } else {
-            if let index = selectedCards.firstIndex(of: cardsOnBoard[index]){
-                selectedCards.remove(at: index)
+            if !selectedCards.contains(cardToChoose) {
+                selectedCards = [cardToChoose]
             } else {
-                selectedCards.append(cardsOnBoard[index])
+                selectedCards = []
+            }
+            selectedCardsMatched = false
+        }
+        // 1 or 2 crads are selected
+        else {
+            // if the card we are trying to select is already selected, we deselect it
+            if let index = selectedCards.firstIndex(of: cardToChoose){
+                selectedCards.remove(at: index)
+            }
+            // else, we add the card to selected cards
+            else {
+                selectedCards.append(cardToChoose)
+                // if after selecting a crad, there are 3 selected cards,
+                // we check wether selected cards match
                 if(selectedCards.count == 3) {
-                    selectedCardsMatched = checkIfSelectedcCardsMatch()
+                    selectedCardsMatched = checkIfSelectedcCardsMatch(cards: selectedCards)
                 }
             }
         }
     }
-    private func replaceMatchedCards(){
-        for card in selectedCards {
+    public func replaceMatchedCards(chosenCards: Array<Card>){
+        for card in chosenCards {
             if let indexOfCard = cardsOnBoard.index(of: card) {
                 // if there are more cards in the deck
                 if deck.count > 0 {
@@ -50,14 +63,14 @@ class SetGame {
             }
         }
     }
-    private func checkIfSelectedcCardsMatch() -> Bool {
+    public func checkIfSelectedcCardsMatch(cards: Array<Card>) -> Bool {
         var attributesMapsArray = [
             [Int: Bool](),
             [Int: Bool](),
             [Int: Bool](),
             [Int: Bool]()
         ]
-        for card in selectedCards {
+        for card in cards {
             var attributeType = 0
             for attribute in card.attributes {
                 if attributesMapsArray[attributeType][attribute] == nil {
@@ -91,7 +104,7 @@ class SetGame {
         createDeck()
         deck.shuffle()
         dealTwelveCards()
-        selectedCardsMatched = false
+        selectedCardsMatched = nil
     }
     private func createDeck(){
         for shape in 0...2 {
@@ -110,4 +123,25 @@ class SetGame {
         }
     }
 }
+#if DEBUG
+extension SetGame {
+    func setDeck(newDeck: [Card]){
+        self.deck = newDeck
+    }
+    func setCardsOnBoard(newCardsOnBoard: [Card]){
+        self.cardsOnBoard = newCardsOnBoard
+    }
+    func setSelectedCards(newSelectedCards: [Card]){
+        self.selectedCards = newSelectedCards
+    }
+    func createNonShuffledDeckForTest() {
+        deck = []
+        self.createDeck()
+    }
+    func dealTwelveCardsForTest() {
+        self.dealTwelveCards()
+    }
+}
+#endif
+
 
